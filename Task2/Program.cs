550a2fecb228b09abd169b0f7d7c956ec4ca20a7
denis.tsapenko <denis.tsapenko90@yandex.ru>
2022-08-13 14:33:09 +0300

    Решение задач

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,3 +2,16 @@
 //452 -> 11
 //82 -> 10
 //9012 -> 12
+
+
+Console.Write("Число ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
+
+while (a > 0)
+{
+b = b + a % 10;
+a = a/10 ;
+}
+Console.WriteLine($"Cумма чисел равна: {b}");
+return 0;
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,3 +1,48 @@
 ﻿//Напишите программу, которая создает массив из введенных через запятую цифр и выводит этот массив на экран.
 //1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 //6, 1, 33 -> [6, 1, 33]
+
+//Заполнения массива из строки  
+Console.Write("Введите числа: ");
+string? seriesOfNumbers = Console.ReadLine();
+seriesOfNumbers = seriesOfNumbers + ",";
+
+// заполняет массив значениями из строки
+int[] ArrayOfNumbers(string seriesNew)
+{ 
+    int[] arrayOfNumbers = new int[1];    
+    int j = 0;
+    for (int i = 0; i < seriesNew.Length; i++)
+    {
+        string seriesNew1 = "";
+        while (seriesNew[i] != ',' && i < seriesNew.Length)
+        {
+            seriesNew1 += seriesNew[i];
+            i++;
+            }
+    arrayOfNumbers[j] = Convert.ToInt32(seriesNew1);    
+    if (i < seriesNew.Length-1)
+    {
+        arrayOfNumbers = arrayOfNumbers.Concat(new int[] {0}).ToArray();
+    }
+    j++;
+  }
+  return arrayOfNumbers;
+}
+// Вывод массива на печать 
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 0;
+  Console.Write("[");
+  while(index < count){
+    Console.Write(coll[index]);
+    index++;
+    if (index < count){
+      Console.Write(", ");
+    }
+  }
+  Console.Write("]");
+} 
+
+int[] arrayOfNumbers =  ArrayOfNumbers(seriesOfNumbers);
+PrintArry(arrayOfNumbers);
